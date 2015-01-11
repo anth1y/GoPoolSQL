7fd2bf24b58eff742d091ec3ef8db0093d161d5c
ant <anth1y@gmail.com>
2015-01-11 01:05:19 +0000

    initial commit

---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/lib/pq"
+)
+
+func main() {
+	//	server
+	// have all incoming requests listen on multiple channels
+	// then hand off to the postgres master
+}
