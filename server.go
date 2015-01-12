2cc4380ad7ffab9af62ef51bbebbdec16ce7a337
ant <anth1y@gmail.com>
2015-01-12 05:55:28 +0000

    adding some networking libs and a nice framework

--- server.go
+++ server.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/lib/pq"
+	"net"
 )
 
 func main() {
+	ln, err := net.Listen("tcp", ":5432")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
 	//	server
 	// have all incoming requests listen on multiple channels
 	// then hand off to the postgres master
