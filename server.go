8cb8187a1507c95005dba4dbc513a097fbc5371f
ant <anth1y@gmail.com>
2015-02-20 05:59:09 +0000

    Trying to add more rows to the db

--- server.go
+++ server.go
@@ -6,34 +6,33 @@ import (
         "database/sql"
 	_ "github.com/lib/pq"
         "time"
+        "os"
 )
 
 func main() {
         // create a statement string
-        var sStmt string = "insert into test (gopher_id, created) values ($1, $2)"
+      var sStmt string = "insert into test (gopher_id, created) values ($1, $2)"
+      var entries int = 100000
         //This is to open the db things a bit but will not open till first req. 
-	db, err := sql.Open("postgres",  "host=/var/run/postgresql dbname=testdb user=ant sslmode=disable")
+      db, err := sql.Open("postgres",  "host=/var/run/postgresql dbname=testdb user=ant sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+     fmt.Printf("StartTime: %v\n", time.Now())
+     for i := 0; i < entries; i++ { 
 
        stmt, err := db.Prepare(sStmt)
 	if err != nil {
-		log.Fatal(err)
+	  log.Fatal(err)
 	}
-
-       fmt.Printf("StartTime: %v\n", time.Now())
-
-//       res, err := stmt.Exec(1, time.Now())
-//	if err != nil {
-//		log.Fatal(err)
-//	}
 
         // close statement
         stmt.Close()
-        
-        // close db
-        db.Close()
+    } 
+    // close db
+    db.Close()
          
-       fmt.Printf("StopTime: %v\n", time.Now())
+   fmt.Printf("StopTime: %v\n", time.Now())
+   os.Exit(0)
 }
