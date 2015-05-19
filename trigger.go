891e3dd2342e5345f52d70d0a81ae3b1f2843b48
ant <anth1y@gmail.com>
2015-05-18 20:56:04 -0700

    Adding some capability to read files

--- trigger.go
+++ trigger.go
@@ -1,8 +1,15 @@
-package main
+package trigger
 
 import (
-  ""
+	"fmt"
+	"io/ioutil"
 )
-func main{
-  // This file would be to move the recovery.conf file from slave to another slave.
+
+func trigger() {
+	if _, err := os.Stat(recovery.conf); os.IsNotExist(err) {
+		fmt.Printf("I'm not the master shonuff")
+		return
+	}
+	// This file would be to move the recovery.conf file from slave to another slave.
+	// make agents to talk to each other and if necessary write recovery.conf
 }
